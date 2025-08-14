73d85b6af1ab0cb04c8aa680636079ddbf621829
alumno <alumno@Instituto.local>
2025-08-14 11:19:54 -0300

    apis

---
+++ index.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('#titulo')
+let imagen = document.querySelector('#imagen')
+let boton = document.querySelector('#boton')
+
+boton.onclick = function () {
+    fetch('https://pokeapi.co/api/v2/pokemon-form/157/')
+
+
+}

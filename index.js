065cf0de37267f478166779a659bd92a675c36ef
GonzaSai <136342939+GonzaSai@users.noreply.github.com>
2025-08-17 16:35:18 -0300

    APIS

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let boton = document.querySelector('#boton')
 
 boton.onclick = function () {
     fetch('https://pokeapi.co/api/v2/pokemon-form/157/')
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        console.log(datos);
+        titulo.textContent = datos.name + ' #' + datos.order;
+        imagen.src = datos.sprites.back_default;
+    })
 
 
 }
